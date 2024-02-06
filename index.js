f34277504db4f4ffec3fc725f9d2617ea0915c9f
Ahmed Shalash <ah.shalash@gmail.com>
2024-02-06 04:16:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your code here
+// Class for Breakfast
+class Breakfast {
+    constructor(food, drink) {
+      this.food = food;
+      this.drink = drink;
+    }
+  }
+  
+  // Class for Lunch
+  class Lunch {
+    constructor(salad, soup, drink) {
+      this.salad = salad;
+      this.soup = soup;
+      this.drink = drink;
+    }
+  }
+  
+  // Class for Dinner
+  class Dinner {
+    #dessert; // Private field for dessert
+  
+    constructor(salad, soup, entree, dessert) {
+      this.salad = salad;
+      this.soup = soup;
+      this.entree = entree;
+      this.#dessert = dessert;
+    }
+  }
+  
